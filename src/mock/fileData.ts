3cac94857f41bfdd3a81d4bbe18b6628668c2111
guruguru <lhtisgood@163.com>
2025-10-20 14:44:51 +0800

    chore:Refactor JSX filenames and update values

--- src/mock/fileData.ts
+++ src/mock/fileData.ts
@@ -1,9 +1,9 @@
 export const templates: any = [
   {
-    filename: 'src',
-    value: '',
-    path: 'src',
-    kind: 'directory',
+    filename: "src",
+    value: "",
+    path: "src",
+    kind: "directory",
     children: [
       // {
       //   filename:'utils.js',
@@ -16,36 +16,53 @@ export const templates: any = [
       //   `,
       // },
       {
-        filename: 'app.jsx',
-        value: `    import A from './extraA.jsx';
-    import B from './extraB.jsx';
+        filename: "app.jsx",
+        value: `    import Cmpt1 from './component1.jsx';
+    import Cmpt2 from './component2.jsx';
   
     function App() {
         const [txt,setTxt] = useState(0)
-        const getCall = ()=>{alert('change text!');setTxt(txt+1)}
+        const getCall = ()=>{alert('changwe text!');setTxt(txt+1)}
         let style ='important!';
         return (
           <>
-              <A />
-              <div onClick={() => getCall()}>try click it{txt}</div>
-              <p>other component:</p>
-              <B />
+          <h2 style={{'textAlign':'center'}}>This is React Page</h2>
+          <div className="box">
+            <div className="inner-box"></div>
+          </div>
+          <h3>Ant Design components below:</h3>
+            <div style={{"padding":"10px"}}>
+              <Button type='primary'>hello</Button>
+            </div>
+            <div style={{"padding":"10px"}}>
+              <Card title="Card" size="small">
+                <p>Card content</p>
+                <p>Card content</p>
+              </Card>
+            </div>
+            <div style={{"padding":"10px"}}>
+              <Pagination defaultCurrent={1} total={50} />
+            </div>
+              <h3>Other components:</h3>
+              <Cmpt1/>
+              <Cmpt2 />
           </>
         );
       }
     export default App;`,
-        path: 'src/app.jsx',
+        path: "src/app.jsx",
       },
       {
-        filename: 'extraA.jsx',
-        value: `   function ExtraA() {
+        filename: "component1.jsx",
+        value: `     function ExtraA() {
       const [txt,setTxt] = useState(0)
       const getCall = ()=>{alert('change text!');setTxt(txt+1)}
       return (
         <div className="App-inner">
         <header className="App-header">
+          <button>click</button>
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src App.js</code> and save to reload.
           </p>
           <a
             className="App-link" 
@@ -61,10 +78,10 @@ export const templates: any = [
       );
     }
   export default ExtraA`,
-        path: 'src/extraA.jsx',
+        path: "src/component1.jsx",
       },
       {
-        filename: 'extraB.jsx',
+        filename: "component2.jsx",
         value: `   function ExtraB() {
         const [txt,setTxt] = useState(0)
         const getCall = ()=>{alert('wasai');setTxt(txt+1)}
@@ -73,7 +90,7 @@ export const templates: any = [
         );
       }
     export default ExtraB`,
-        path: 'extraB.jsx',
+        path: "component2.jsx",
       },
     ],
   },
