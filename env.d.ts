e480680d3b54ae121558d8b214046c1bdfa2264c
daryl_franxx <lhtisgood@outlook.com>
2025-12-22 12:54:18 +0800

    chore:调整模版样式及declare文件

--- env.d.ts
+++ env.d.ts
@@ -2,3 +2,10 @@ declare module '@/components/*' {
 	const reactComponent: any;
 	export default reactComponent;
 }
+
+declare interface Window {
+	myCustomFunction: (arg: string) => void;
+	_isAssistanceMode: boolean;
+	_assistanceTempate: any;
+	_canSelectTree: boolean;
+}
